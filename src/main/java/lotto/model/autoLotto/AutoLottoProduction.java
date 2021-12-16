b768af84770264e8b8fbc00f96ff3de37d6ee9ea
her0807 <her0807@naver.com>
2021-12-16 15:38:56 +0900

    refactor: 로또 생성에 관한 패키지 생성

--- src/main/java/lotto/model/autoLotto/AutoLottoProduction.java
+++ src/main/java/lotto/model/autoLotto/AutoLottoProduction.java
@@ -1,4 +1,4 @@
-package lotto.model;
+package lotto.model.autoLotto;
 
 import java.util.HashSet;
 import java.util.LinkedList;
--- src/main/java/lotto/model/autoLotto/LottoNumber.java
+++ src/main/java/lotto/model/autoLotto/LottoNumber.java
@@ -1,4 +1,4 @@
-package lotto.model;
+package lotto.model.autoLotto;
 
 public class LottoNumber {
     private final int number;
